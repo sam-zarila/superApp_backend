d91a4151fd25ba0087bc9b6ef55b20747adee6cf
sam-zarila <zarilatroxx99@gmail.com>
2024-12-29 15:53:40 -0800

    getting started with market place

--- src/marketplace/marketplace.controller.ts
+++ src/marketplace/marketplace.controller.ts
@@ -2,10 +2,22 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { MarketplaceService } from './marketplace.service';
 import { CreateMarketplaceDto } from './dto/create-marketplace.dto';
 import { UpdateMarketplaceDto } from './dto/update-marketplace.dto';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiBody, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { Marketplace } from './entities/marketplace.entity';
 
 @ApiTags('MarketPlace Endpoint')
 @Controller('marketplace')
 export class MarketplaceController {
- 
+  constructor( private readonly marketplaceservice:MarketplaceService){}
+
+  @Post()
+  @ApiOperation({summary:'posting marketplace item'})
+  @ApiBody({type:Marketplace})
+
+  postMarketplaceItem(@Body() marketplace:Marketplace){
+
+    return this.marketplaceservice.createMarketPlaceItem(marketplace)
+
+  }
+  @Get()
 }
