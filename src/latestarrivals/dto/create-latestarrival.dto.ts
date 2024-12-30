e12b9d9539f44278ba8423f7cb10689caf03ccc1
sam-zarila <zarilatroxx99@gmail.com>
2024-12-30 00:43:21 -0800

    latest arrivals

--- src/latestarrivals/dto/create-latestarrival.dto.ts
+++ src/latestarrivals/dto/create-latestarrival.dto.ts
@@ -1,1 +1,3 @@
-export class CreateLatestarrivalDto {}
+export class CreateLatestarrivalDto {
+    
+}
--- src/latestarrivals/entities/latestarrival.entity.ts
+++ src/latestarrivals/entities/latestarrival.entity.ts
@@ -1,1 +1,23 @@
-export class Latestarrival {}
+import { ApiProperty } from "@nestjs/swagger";
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Latestarrival {
+
+    @ApiProperty()
+    @PrimaryGeneratedColumn()
+    id:number
+
+    @ApiProperty()
+    @Column()
+    image:string
+
+
+    @ApiProperty()
+    @Column()
+    name:string
+
+    @ApiProperty()
+    @Column()
+    price:number;
+}
