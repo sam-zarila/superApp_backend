f3c91505d9e029980386a72be2a18c8932687d9e
sam-zarila <zarilatroxx99@gmail.com>
2024-12-30 00:52:52 -0800

    latest arrivals

--- src/latestarrivals/latestarrivals.service.ts
+++ src/latestarrivals/latestarrivals.service.ts
@@ -1,6 +1,14 @@
+
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Latestarrival } from './entities/latestarrival.entity';
+import { Repository } from 'typeorm';
 
 
 @Injectable()
 export class LatestarrivalsService {
+
+  constructor( @InjectRepository(Latestarrival)  private latestArrivals:Repository<Latestarrival> ){}
+  
+
 }
