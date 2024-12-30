417a9803607653d6ba8e0d4314139f0d77926150
sam-zarila <zarilatroxx99@gmail.com>
2024-12-30 00:47:15 -0800

    latest arrivals

--- src/latestarrivals/latestarrivals.module.ts
+++ src/latestarrivals/latestarrivals.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { LatestarrivalsService } from './latestarrivals.service';
 import { LatestarrivalsController } from './latestarrivals.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Latestarrival } from './entities/latestarrival.entity';
 
 @Module({
+  imports:[TypeOrmModule.forFeature([Latestarrival])],
   controllers: [LatestarrivalsController],
   providers: [LatestarrivalsService],
 })
