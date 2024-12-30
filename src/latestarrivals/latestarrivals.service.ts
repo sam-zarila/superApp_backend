51f4fb99204ef7e4dcd04db319f59317213a3b64
sam-zarila <zarilatroxx99@gmail.com>
2024-12-30 01:08:01 -0800

    latest arrivals

--- src/latestarrivals/latestarrivals.service.ts
+++ src/latestarrivals/latestarrivals.service.ts
@@ -1,5 +1,6 @@
 
 
+
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Latestarrival } from './entities/latestarrival.entity';
@@ -20,5 +21,16 @@ export class LatestarrivalsService {
     return this.latestArrivalsRepository.find()
   }
   
+  async UpdateLatestArrivaltem( id:number, updatedData:Partial<Latestarrival>){
+    await this.latestArrivalsRepository.update(id,updatedData)
+
+    return this.latestArrivalsRepository.findOneBy({id})
+  }
+
+  async removeLatestArrivallItem(id:number):Promise<void>{
+
+    await this.latestArrivalsRepository.delete({id})
+
+  }
 
 }
