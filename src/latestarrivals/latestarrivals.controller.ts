a9658cf0c589cee81a3eda1d1c4d045d3133e184
sam-zarila <zarilatroxx99@gmail.com>
2024-12-30 01:29:12 -0800

    latest arrivals

--- src/latestarrivals/latestarrivals.controller.ts
+++ src/latestarrivals/latestarrivals.controller.ts
@@ -3,10 +3,15 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { LatestarrivalsService } from './latestarrivals.service';
 import { CreateLatestarrivalDto } from './dto/create-latestarrival.dto';
 import { UpdateLatestarrivalDto } from './dto/update-latestarrival.dto';
+import { ApiOperation } from '@nestjs/swagger';
 
 @Controller('latestarrivals')
 export class LatestarrivalsController {
   constructor(
     private readonly LatestArrivalsService:LatestarrivalsService
   ){}
+  @Post()
+  @ApiOperation({summary:"posting latest arrival"})
+  @
+
 }
