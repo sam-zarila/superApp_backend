669e7b02cc0fcaf5a92025da6159e4eab1f18260
sam-zarila <zarilatroxx99@gmail.com>
2025-01-01 02:57:43 -0800

    cart functionality

--- src/cart/cart.service.ts
+++ src/cart/cart.service.ts
@@ -1,7 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import { CreateCartDto } from './dto/create-cart.dto';
 import { UpdateCartDto } from './dto/update-cart.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { cartEntity } from './entities/cart.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class CartService {
+
+  constructor (
+   @InjectRepository(cartEntity) private readonly cartRepository:Repository<cartEntity>
+  ){}
+
+  async addToCart( addTocart:CreateCartDto) :Promise<{message:string}>{
+
+  }
 }
