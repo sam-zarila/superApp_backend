5f2d1e9f724abacde05c6581ef223030cb5df157
sam-zarila <zarilatroxx99@gmail.com>
2025-01-09 14:47:24 -0800

    cartpage

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,6 +5,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/entities/user.auth_entity';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from 'src/jwt/jwtstrategy';
 
 @Module({
   imports:[
@@ -16,6 +17,7 @@ import { JwtModule } from '@nestjs/jwt';
     
     TypeOrmModule.forFeature([User])],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService,JwtStrategy],
+  exports:[JwtModule,PassportModule]
 })
 export class AuthModule {}
