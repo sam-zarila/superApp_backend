2372b454e73456a00c2fa1c91a673d5e624c5404
sam-zarila <zarilatroxx99@gmail.com>
2025-01-14 16:51:52 -0800

    fixing cart

--- src/jwt/jwt.module.ts
+++ src/jwt/jwt.module.ts
@@ -1,4 +1,16 @@
 import { Module } from '@nestjs/common';
+import { JwtModule as NestJwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './jwtstrategy';
 
-@Module({})
+@Module({
+  imports: [
+    
+    NestJwtModule.register({
+      secret: 'your-secret-key', // Hardcoded secret key
+      signOptions: { expiresIn: '1h' }, // Token expiration time
+    }),
+  ],
+  providers: [JwtStrategy],  // Provide the JwtStrategy for use in other services
+  exports: [NestJwtModule], // Export the configured JwtModule for use in other modules
+})
 export class JwtModule {}
