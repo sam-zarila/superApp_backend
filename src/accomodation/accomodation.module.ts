6e8cf1477dc94ba84fe111a8acc44d646af37d9b
sam-zarila <zarilatroxx99@gmail.com>
2025-01-15 16:20:56 -0800

    accomodation

--- src/accomodation/accomodation.module.ts
+++ src/accomodation/accomodation.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AccomodationController } from './controllers/accomodation/accomodation.controller';
 import { AccomodationService } from './services/accomodation/accomodation.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { BookingRoom } from 'src/entities/Booking.Entity';
 
 @Module({
+  imports:[TypeOrmModule.forFeature([BookingRoom])],
   controllers: [AccomodationController],
   providers: [AccomodationService]
 })
