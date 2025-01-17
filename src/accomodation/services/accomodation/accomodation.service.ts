1d11da1595c16b3075ac2e8675387af93aa2956a
sam-zarila <zarilatroxx99@gmail.com>
2025-01-16 16:00:16 -0800

    accomodation

--- src/accomodation/services/accomodation/accomodation.service.ts
+++ src/accomodation/services/accomodation/accomodation.service.ts
@@ -1,4 +1,30 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { BookingRoom } from 'src/entities/Booking.Entity';
+import { BoardingHouse } from 'src/entities/Hostel.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class AccomodationService {}
+export class AccomodationService {
+  constructor(
+    @InjectRepository(BookingRoom) private bookingRoomRepository: Repository<BookingRoom>,
+    @InjectRepository(BoardingHouse) private boardingHouseRepository: Repository<BoardingHouse>
+
+  ){}
+
+  private async generateUniqueOderNumber(): Promise<String>{
+     
+    let isUnique= false;
+    let BookingNumber: string;
+
+    while (!isUnique) {
+
+        const randomNumber = Math.floor(1000 + Math.random() * 9000);
+        BookingNumber = `SuperApp${randomNumber}`;
+        
+    }
+
+  }
+
+
+}
